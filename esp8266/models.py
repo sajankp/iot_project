419a2aa939b0bf12d396ba259b47af56589bbbb1
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-02-25 18:27:44 +0530

    added few changes necessary for creating two fields day, month, year

--- esp8266/models.py
+++ esp8266/models.py
@@ -7,6 +7,19 @@ import datetime
 class Data(models.Model):
     value = models.CharField(max_length = 64)
     date = models.DateTimeField()
+
+    @property
+    def month(self):
+        return self.date.month
+
+    @property
+    def year(self):
+        return self.date.year
+
+    @property
+    def day(self):
+        return self.date.day
+
     def __str__(self):
         date = datetime.datetime.strftime(self.date,"%D - %T")
         return f"{self.value} recorded on {date}"
