246b1f155af22d6b742d81babb664ac315064c76
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-02-26 18:37:10 +0530

    added month field to api

--- esp8266/serializers.py
+++ esp8266/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class DataSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Data
-        fields = ('value', 'date')
+        fields = ('value', 'date', 'month')
