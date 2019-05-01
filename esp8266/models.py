c469221d3ae076d69c67b3788133557368232f14
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-05-01 08:19:44 +0530

    Update model to include Humidity value
    The model have been updated to change temperature to a integerfield with validator to remove impossible values from ever getting saved.
    Humidity field was added with similair constraints and __str__ representation was updated as well.
    update db fields with necessary default values

--- esp8266/models.py
+++ esp8266/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from datetime import datetime
 from django.utils import timezone
+from django.core.validators import MaxValueValidator
 # Create your models here.
 import datetime
 
 class Data(models.Model):
-    value = models.CharField(max_length = 64)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default = timezone.now)
+    temperature = models.PositiveSmallIntegerField(validators=[MaxValueValidator(150)],default = 0)
+    humidity = models.PositiveSmallIntegerField(validators=[MaxValueValidator(150)],default = 0)
 
     @property
     def month(self):
@@ -22,4 +24,4 @@ class Data(models.Model):
 
     def __str__(self):
         date = datetime.datetime.strftime(self.date,"%D - %T")
-        return f"{self.value} recorded on {date}"
+        return f"Temperature: {self.temperature} and Humidity: {self.humidity} recorded on {date}"
