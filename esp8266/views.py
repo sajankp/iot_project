c63e1fb5a58a3a1dd5d5bf4a7ddcd2fbb333f8cf
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-05-01 09:06:15 +0530

    Add temp and humidity to post view and in index page template

--- esp8266/views.py
+++ esp8266/views.py
@@ -10,24 +10,30 @@ from rest_framework import viewsets
 from esp8266.serializers import DataSerializer
 # Create your views here.
 
+
 @csrf_exempt
 def store(request):
-    a = json.loads(request.body)['temp']
-    value = Data(value = a,date = timezone.localtime(timezone.now()))
+    temp = json.loads(request.body)['temp']
+    humid = json.loads(request.body)['humidity']
+    value = Data(temperature=temp, humidity=humid,
+                 date=timezone.localtime(value=timezone.now()))
     value.save()
-    return HttpResponse(f'Done and recieved {value.value} at {value.date}')
+    return HttpResponse(value)
+
 
 def index(request):
     values = Data.objects.all().order_by('date')
-    context = {"values" : values}
-    return render(request,"esp8266/values.html",context)
+    context = {"values": values}
+    return render(request, "esp8266/values.html", context)
+
 
 def chart(request):
     now_in_timezone = timezone.now()
-    one_hour_ago = now_in_timezone - datetime.timedelta(seconds = 60*60)
-    values = Data.objects.filter(date__range=[one_hour_ago,now_in_timezone])
-    context = {"values" : values}
-    return render(request,"esp8266/chart.html",context)
+    one_hour_ago = now_in_timezone - datetime.timedelta(seconds=60*60)
+    values = Data.objects.filter(date__range=[one_hour_ago, now_in_timezone])
+    context = {"values": values}
+    return render(request, "esp8266/chart.html", context)
+
 
 class DataViewSet(viewsets.ModelViewSet):
     """
