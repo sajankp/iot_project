8b894ccf5d8f53de07e86fdb21498291026d55ed
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-05-16 15:58:42 +0530

    Update settings for travis

--- iot/settings.py
+++ iot/settings.py
@@ -20,8 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('iot/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+if os.getenv('BUILD_ON_TRAVIS', None):
+    SECRET_KEY = "SecretKeyForUseOnTravis"
+else:
+    with open('iot/secret_key.txt') as f:
+        SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
