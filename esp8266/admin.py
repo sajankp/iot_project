c76e02ab4f17a7d0ecf2fba9d4522bc31570c8c5
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-06-10 21:14:17 +0530

    Update admin page with filtering option

--- esp8266/admin.py
+++ esp8266/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Data
 # Register your models here.
 
-admin.site.register(Data)
+@admin.register(Data)
+class DataAdmin(admin.ModelAdmin):
+    list_display = ['date','humidity','temperature']
+    fields = ['Date','Humidity','Temperature']
+    list_filter = ['date']
