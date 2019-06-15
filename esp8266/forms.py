8cc00d74f4d2bf3f6ec0d56198c94ea1109cc760
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-06-15 21:00:25 +0530

    Create new page with chart of max & min values based on form input.
    update the graph to grouped bar chart, and display date based max and minimum values for different options

--- esp8266/forms.py
+++ esp8266/forms.py
@@ -7,3 +7,25 @@ class DateForm(forms.Form):
                            localize=True,
                            widget=forms.TextInput(attrs={'class': 'date-input'}),
                            )
+
+class DataFilterForm(forms.Form):
+    week='wk'
+    month='mh'
+    year='yr'
+    TYPE_OF_READING=[
+        (1,"Temperature"),
+        (2,"Humidity"),
+    ]
+    TYPE_OF_FILTER=[
+        (week,"Week"),
+        (month,"Month"),
+        (year,"Year"),
+    ]
+    reading = forms.ChoiceField(label="Temperature or Humidity",
+                                choices=TYPE_OF_READING,
+                                initial=1
+                                )
+    filter = forms.ChoiceField(label="Time Filter",
+                              choices=TYPE_OF_FILTER,
+                              initial='wk'
+                             )
