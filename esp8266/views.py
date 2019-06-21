d003c14a0c43914fb20989a227f8aa7f7e75ea8e
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-06-21 23:44:03 +0530

    Add sensor detection and security into /post view and in microcontroller
    By using uuid for identification and if not present will render the device into sleep.
    Restructured most of the view code

--- esp8266/views.py
+++ esp8266/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from .models import Data,Sensor,Appartement,Building
+from .models import Data, Sensor, Appartement, Building
 from django.utils import timezone
 from django.urls import reverse
-import json,collections
+import json
+import collections
 from rest_framework import viewsets
 from esp8266.serializers import DataSerializer
-from esp8266.forms import DateForm,DataFilterForm
+from esp8266.forms import DateForm, DataFilterForm
 from django.views import generic
 from itertools import groupby
 # Create your views here.
@@ -15,18 +16,23 @@ from itertools import groupby
 
 @csrf_exempt
 def store(request):
-    if json.loads(request.body)['ERROR_IN_MEASUREMENT'] == True:
-        return JsonResponse({'message':"Error in value hence not recorded"})
-    if json.loads(request.body)['FIRST'] == True:
-        a=timezone.now()
-        return JsonResponse({'time':(14-a.minute%15)*60+(90-a.second),
-                            'message':"Reading will be recorded next time, now time is "+str(a)})
-    temp = json.loads(request.body)['temp']
-    humid = json.loads(request.body)['humidity']
+    recieved_json = json.loads(request.body)
+    try:
+        sensor = Sensor.objects.filter(key__exact=recieved_json['secretkey'])[0]
+    except:
+        return JsonResponse({'message': "wrong key or sensor is not registered", 'time': 1000000})
+    if recieved_json['ERROR_IN_MEASUREMENT']:
+        return JsonResponse({'message': "Error in value hence not recorded"})
+    if recieved_json['FIRST']:
+        a = timezone.localtime()
+        return JsonResponse({'time': (29 - a.minute % 30) * 60 + (90 - a.second),
+                            'message': "Reading will be recorded next time, now time is "+str(a)})
+    temp = recieved_json['temp']
+    humid = recieved_json['humidity']
     value = Data(temperature=temp, humidity=humid,
-                 date=timezone.localtime(value=timezone.now()))
+                 date=timezone.localtime(value=timezone.now()), sensor=sensor)
     value.save()
-    return JsonResponse({'message':str(value)})
+    return JsonResponse({'message': str(value)})
 
 
 class DataViewSet(viewsets.ModelViewSet):
@@ -37,7 +43,7 @@ class DataViewSet(viewsets.ModelViewSet):
     serializer_class = DataSerializer
 
 
-def graph_per_day(request,pk):
+def graph_per_day(request, pk):
     if not request.GET:
         form = DateForm()
     else:
@@ -45,9 +51,10 @@ def graph_per_day(request,pk):
     sensor = Sensor.objects.get(pk=pk)
     data = Data.objects.filter(sensor__exact=sensor)
     values = data.filter(date__date=form['date'].value())
-    return render(request, 'esp8266/graph_per_day.html', {'form': form, 'values':values, 'count':len(values), "sensor":sensor})
+    return render(request, 'esp8266/graph_per_day.html', {'form': form, 'values': values, 'count': len(values), "sensor": sensor})
 
-def graph_max_min(request,pk):
+
+def graph_max_min(request, pk):
     """
     The filtered data based on form input with minimum and maximum of temp/humidity
     """
@@ -57,23 +64,23 @@ def graph_max_min(request,pk):
         form = DataFilterForm(request.GET)
     sensor = Sensor.objects.get(pk=pk)
     datas = Data.objects.filter(sensor__exact=sensor)
-    if form['filter'].value()=='mh':
+    if form['filter'].value() == 'mh':
         values = datas.filter(date__month=timezone.localdate().month)
-    elif form['filter'].value()=='yr':
+    elif form['filter'].value() == 'yr':
         values = datas.filter(date__year=timezone.localdate().year)
     else:
         values = datas.filter(date__week=timezone.localdate().isocalendar()[1])
     if form['reading'].value() == '1':
-        b={(timezone.localdate(x.date),x.temperature) for x in values}
+        b = {(timezone.localdate(x.date), x.temperature) for x in values}
     else:
-        b={(timezone.localdate(x.date),x.humidity) for x in values}
-    b=sorted(list(b))
-    max_values = (max(list(group)) for key, group in groupby(b,key=lambda x:x[0]))
-    min_values = (min(list(group)) for key, group in groupby(b,key=lambda x:x[0]))
-    combined_values = zip(max_values,min_values)
-    reading=collections.namedtuple('Reading',['date','max_reading','min_reading'])
-    values = [reading(x[0][0],x[0][1],x[1][1]) for x in combined_values]
-    return render(request,'esp8266/graph_max_min.html',{"form":form,"values":values,'sensor': sensor})
+        b = {(timezone.localdate(x.date), x.humidity) for x in values}
+    b = sorted(list(b))
+    max_values = (max(list(group)) for key, group in groupby(b, key=lambda x: x[0]))
+    min_values = (min(list(group)) for key, group in groupby(b, key=lambda x: x[0]))
+    combined_values = zip(max_values, min_values)
+    reading = collections.namedtuple('Reading', ['date', 'max_reading', 'min_reading'])
+    values = [reading(x[0][0], x[0][1], x[1][1]) for x in combined_values]
+    return render(request, 'esp8266/graph_max_min.html', {"form": form, "values": values, 'sensor': sensor})
 
 
 class SensorDetailView(generic.DetailView):
@@ -96,11 +103,11 @@ def index(request):
     num_datas = Data.objects.all().count()
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
-    context = {"buildings":buildings,
-               "num_datas":num_datas,
-               "num_sensors":num_sensors,
-               "num_appartements":num_appartements,
-               "num_buildings":num_buildings,
-               "num_visits":num_visits,
+    context = {"buildings": buildings,
+               "num_datas": num_datas,
+               "num_sensors": num_sensors,
+               "num_appartements": num_appartements,
+               "num_buildings": num_buildings,
+               "num_visits": num_visits,
                }
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
