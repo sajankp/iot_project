10df164d5f77c635c6dcf4afe484a4e9fac50f4c
sajankp <skpsajan@gmail.com>
2021-07-18 02:13:33 +0530

    Refactor code.
    Small fixes with no code impact

--- websocket_test_existing db.py
+++ websocket_test_existing db.py
@@ -1,9 +1,11 @@
 import json
-import asyncio
+
 import websockets
+
+
 async def hello():
-    async with websockets.connect('ws://35.244.13.244/ws/post') as websocket:
+    async with websockets.connect('ws://35.244.13.244/ws/post') as web_socket:
         value = int(input("what's the value "))
-        await websocket.send(json.dumps({'temp':value}))
-        greeting = await websocket.recv()
+        await web_socket.send(json.dumps({'temp': value}))
+        greeting = await web_socket.recv()
         return greeting
